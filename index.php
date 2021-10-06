83290d4677697a5cc49609f49864d3a1ad56c0b3
erdisener <erdisener@gmail.com>
2021-10-06 21:49:22 +0300

    title

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Language" content="tr">
     <meta http-equiv="Content-Type" content="text/html; charset=<?php echo Karakter_Seti_Yukle() ?>"/>
-    <title>Otonom Araç Yönetim Sistemi</title>
+    <title>Automated Vehicle Management System</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no"
     />
     <meta name="description" content="Automated Vehicles">
